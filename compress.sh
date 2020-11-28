70fadd2707b9d988686447594937114f4c8c982f
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-11-28 11:28:48 -0500

    process_image_input.py receives command line arguments

--- compress.sh
+++ compress.sh
@@ -5,5 +5,5 @@ then
 	make
 fi
 
-python3 process_image_input.py
+python3 process_image_input.py data/image/input.png data/rgb/test.in
 ./bin/compress data/compressed/test.dat 50 < data/rgb/test.in
--- process_image_input.py
+++ process_image_input.py
@@ -2,17 +2,22 @@ import cv2
 import numpy
 import matplotlib.pyplot as plt
 import os
+import sys
 
-n_img = "data/image/input.png"
+if (len(sys.argv)) != 3: 
+	print("error: missign arguments. Usage: python3", sys.argv[0], "<image_path> <index_path>")
+	exit()
+
+# n_img = "data/image/input.png"
+n_img = sys.argv[1]
 img = cv2.imread(n_img);
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 altura  = img.shape[0]
 ancho = img.shape[1]
 
-os.system("mkdir -p data/rgb");
-
-f = open("data/rgb/test.in", 'w')
+# f = open("data/rgb/test.in", 'w')
+f = open(sys.argv[2], 'w')
 f.write(str(ancho))
 f.write(" ")
 f.write(str(altura) + "\n")
